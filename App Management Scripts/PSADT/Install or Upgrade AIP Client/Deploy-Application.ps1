96170b8f4153d1bd0d6f54344ed7f7ef1deb082a
Andrei Vida-Ratiu <a-avidaratiu@microsoft.com>
2023-03-28 12:10:25 +0300

    Update

--- App Management Scripts/PSADT/Install or Upgrade AIP Client/Deploy-Application.ps1
+++ App Management Scripts/PSADT/Install or Upgrade AIP Client/Deploy-Application.ps1
@@ -227,6 +227,17 @@ Try {
         [String]$installPhase = 'Uninstallation'
 
         Uninstall-AllAipInstances
+        # Remove the client installer registry key. This causes issues sometimes with the client reinstall.
+        Write-Log -Severity 1 -Message 'Removing the client installer registry key.'
+        Push-Location
+        New-PSDrive -PSProvider 'registry' -Root 'HKEY_CLASSES_ROOT' -Name 'HKCR' | Out-Null
+        Set-Location 'HKCR:' | Out-Null
+        $CMCCode = (Get-ItemProperty 'Installer\Products\*' |
+            Where-Object { $_.ProductName -eq 'Microsoft Azure Information Protection' }).PackageCode
+        if (!([string]::IsNullOrWhiteSpace($CMCCode))) {
+            reg.exe delete "HKCR\Installer\Products\$CMCCode" /f
+        }
+        Pop-Location
 
         ##*===============================================
         ##* POST-UNINSTALLATION
